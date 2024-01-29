bcf0291095314b8b68ad00c97dc28519c2ddd64c
Annitas <39757257+Annitas@users.noreply.github.com>
2024-01-29 20:06:50 +0600

    Fix user's view screen

--- app/src/main/java/com/example/juniormathfac/MainActivity.kt
+++ app/src/main/java/com/example/juniormathfac/MainActivity.kt
@@ -3,25 +3,10 @@ package com.example.juniormathfac
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import com.example.juniormathfac.ui.theme.JuniorMathFacTheme
+import com.example.juniormathfac.Views.ProfileView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,74 +16,7 @@ class MainActivity : ComponentActivity() {
                 modifier = Modifier.fillMaxSize(),
             ) {
                 ProfileView()
-
             }
         }
-    }
-}
-
-@Composable
-fun ProfileView(
-    titles: List<String> = listOf("Create Room", "Statistics", "Tasks", "Rules")
-) {
-    Column(
-        modifier = Modifier
-            .padding(vertical = 15.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Text(text = stringResource(R.string.profile_text),
-            fontSize = 30.sp,
-            color = Color.White,
-            modifier = Modifier
-                .drawBehind { drawRoundRect(
-                    Color(0xFFBBAAEE),
-                    cornerRadius = CornerRadius(10.dp.toPx())
-                ) }
-                .padding(horizontal = 60.dp, vertical = 10.dp)
-                .align(alignment = Alignment.CenterHorizontally)
-
-        )
-        Image(
-            painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "User's profile picture",
-            modifier = Modifier
-                .size(160.dp)
-                .clip(CircleShape)
-                .padding(15.dp)
-                .border(1.5.dp, Color.LightGray, CircleShape)
-
-        )
-        Text(text = "Margarita",
-            fontSize = 22.sp
-        )
-        Text(text = "Konstantinopolskaya",
-            fontSize = 22.sp
-        )
-        Spacer(modifier = Modifier.height(40.dp))
-        for (title in titles) {
-            ProfileButton(title)
-        }
-
-    }
-}
-
-@Composable
-fun ProfileButton(title: String) {
-    Button(
-        onClick = { },
-        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Yellow, contentColor = Color.DarkGray),
-        modifier = Modifier
-            .size(width = 260.dp, height = 65.dp)
-            .padding(10.dp)
-    ) {
-        Text(title)
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JuniorMathFacTheme {
-        ProfileView()
     }
 }
